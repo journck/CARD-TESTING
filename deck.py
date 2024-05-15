bc36f4e6e3ed339db4ca30bbcef5b9b77f995432
journck <130095756+journck@users.noreply.github.com>
2024-05-15 18:46:10 -0500

    Modular changing for different decks

--- deck.py
+++ deck.py
@@ -1,20 +1,20 @@
-from card import Card
+from card import *
 
-def create_unshuffled_deck():
+def create_deck(max_value=14):
     deck = []
     suits = ["s", "h", "d", "c"] # spades, hearts, diamonds, clubs
     for suit in suits:
-        for value in range (1, 14):
+        for value in range (1, max_value):
             deck.append(Card(value, suit))
     return deck
 
-perfect_deck = create_unshuffled_deck()
+perfect_deck = create_deck()
 
 def print_deck(deck):
     for card in deck:
         print(card)
 
-def faro_shuffle(deck):
+def faro_shuffle(deck): # assume deck is 52 cards, use 2 piles
     left_deck = []
     right_deck = []
     for c in range(0, 26):
@@ -25,3 +25,20 @@ def faro_shuffle(deck):
         shuffled_deck.append(left_deck[c])
         shuffled_deck.append(right_deck[c])
     return shuffled_deck
+
+def faro_shuffle(deck, num_piles):
+    assert len(deck) % num_piles == 0 # deck is divisible evenly into piles
+
+    # split into piles 
+    pile_size = len(deck) // num_piles
+    piles = []
+    for i in range(num_piles):
+        piles.append(deck[i * pile_size:(i + 1) * pile_size])
+
+    new_shuffled_deck = []
+
+    # interleave piles
+    for i in range(pile_size):
+        for pile in piles:
+            new_shuffled_deck.append(pile[i])
+    return new_shuffled_deck
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import timeit
 import colorsys
 import contextlib
+import random
 
 # local
 from blessed import Terminal
@@ -30,17 +31,46 @@ def lines_from_deck(deck):
     for card in deck:
         line_from_card(card)
 
+def test_equivalence(perfect_deck, questioning_deck):
+    assert len(perfect_deck) == len(questioning_deck)
+    for i in range(len(perfect_deck)):
+        card1 = perfect_deck[i]
+        card2 = questioning_deck[i]
+        if card1.value != card2.value or card1.suit != card2.suit:
+            return False
+    return True
+    
 def main():
-    workingDeck = create_unshuffled_deck()
+    num_shuffles = 0
+    auto_play = False
+    shouldExit = False
+    print(term.clear + term.home)
+    startingDeck = create_deck()
+    workingDeck = startingDeck.copy()
     lines_from_deck(workingDeck)
+    
 
 
     with term.cbreak(), term.hidden_cursor():
-        inp = term.inkey()
-        while term.inkey() not in (u'q', u'Q'):
-            # print(term.home + term.clear)
-            workingDeck = faro_shuffle(workingDeck)
+        ind = term.inkey()
+        if (ind == ' '):
+            auto_play = True
+        while (auto_play or term.inkey() != u'q'):
+            shouldExit = False
+            print(ind)
+            print(term.clear + term.home)
+            workingDeck = faro_shuffle(workingDeck,2)
+            num_shuffles += 1
             lines_from_deck(workingDeck)
+            if test_equivalence(startingDeck, workingDeck):
+                print(term.center("") + term.black_on_green(term.center("THE DECK IS SORTED | SHUFFLES: " + str(num_shuffles))))
+                num_shuffles = 0
+                term.inkey()
+            else:
+                print(term.center("") + term.black_on_red(term.center("THE DECK IS NOT SORTED | SHUFFLES: " + str(num_shuffles))))
+            if auto_play:
+                time.sleep(0.1)
+
 
     main()
 
